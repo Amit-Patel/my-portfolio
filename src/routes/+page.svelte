f16c388fc4319f20e130501eea99c8f7d193d516
Amit-Patel <amitpatel111297@gmail.com>
2024-02-06 23:17:23 -0500

    updating dimensions

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -44,5 +44,5 @@
 		</div>
 	</div>
 	<!-- <Carrousel items={skills ?? MY_SKILLS} /> -->
-	<img src='{base}/images/profile/amit.jpeg' alt="Description of the image">
+	<img src='{base}/images/profile/amit.jpeg' alt="Description of the image" style="width: 150px; height: auto;">
 </div>
